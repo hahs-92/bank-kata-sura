3321bf136140af746e61fda05576f8f34fd24a4d
Alex <alexandre_2420@outlook.com>
2022-04-08 16:14:37 -0500

    added: initial setup

---
+++ src/main/java/com/sofkau/Account.java
@@ -0,0 +1,11 @@
+package com.sofkau;
+
+public class Account {
+
+    void deposit(int amount) {
+    }
+    void withdraw(int amount){
+    }
+    void printStatements() {
+    }
+}
---
+++ src/main/java/com/sofkau/StartApp.java
@@ -0,0 +1,7 @@
+package com.sofkau;
+
+public class StartApp {
+    public static void main(String[] args) {
+
+    }
+}
